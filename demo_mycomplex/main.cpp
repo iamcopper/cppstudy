fcd6ff83a965e8b80512510bb0547fd788bdb1b7
iamcopper <kangpan519@gmail.com>
2020-02-23 10:37:38 +0800

    Add mycomplex class demo.

---
+++ demo_mycomplex/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "mycomplex.hpp"
+
+using namespace std;
+
+ostream&
+operator<< (ostream& os, const mycomplex& x) {
+	return os << '(' << x.real() << ',' << x.imag() << ')';
+}
+
+int main(int argc, char *argv[])
+{
+	mycomplex c1(2, 4);
+	mycomplex c2;
+	cout << "c1 = " <<  c1 << endl;
+	cout << "c2 = " << c2 << endl;
+
+	c2 += c1;
+	c2 += 3;
+
+	c2 = c1 + c2;
+	c2 = 7 + c1;
+	c2 = c1 + 5;
+	c2 = -c1;
+	cout << "c1 = " << c1 << endl;
+	cout << "c2 = " << c2 << endl;
+
+	cout << "(c1 == c2) = "<< (c1 == c2) << endl;
+	cout << "(c1 != c2) = " << (c1 != c2) << endl;
+
+	return 0;
+}
---
+++ demo_mycomplex/mycomplex.hpp
@@ -0,0 +1,74 @@
+#ifndef __COMPLEX_HPP__
+#define __COMPLEX_HPP__
+
+class mycomplex {
+public:
+	mycomplex(double r = 0, double i = 0) :re(r), im(i) { }
+
+	double real() const { return re; }
+	double imag() const { return im; }
+	mycomplex& operator+= (const mycomplex& r);
+	mycomplex& operator+= (const double& r);
+
+private:
+	double re, im;
+
+// 类的友元函数声明:
+// 友元函数声明可以放在类内任意位置；
+// 声明后该函数就可以直接操作类的Private数据域；
+friend mycomplex& __doapl(mycomplex& ths, const mycomplex& r);
+};
+
+inline mycomplex&
+__doapl(mycomplex& l, const mycomplex& r) {
+	l.re += r.re;
+	l.im += r.im;
+	return l;
+}
+
+// 操作符重载-类成员函数
+// 返回引用：将传入的数据，再以引用返回
+inline mycomplex&
+mycomplex::operator+= (const mycomplex& r) {
+	return __doapl(*this, r);
+}
+
+inline mycomplex&
+mycomplex::operator+= (const double& r) {
+	this->re += r;
+	return *this;
+}
+
+// 操作符重载-类非成员函数
+// 不可返回引用：不可将临时对象作为引用返回
+inline mycomplex
+operator+ (const mycomplex& x, const mycomplex& y) {
+	return mycomplex(x.real() + y.real(), x.imag() + y.imag());
+}
+
+inline mycomplex
+operator+ (const double& x, const mycomplex& y) {
+	return mycomplex(x + y.real(), y.imag());
+}
+
+inline mycomplex
+operator+ (const mycomplex& x, const double& y) {
+	return mycomplex(x.real() + y, x.imag());
+}
+
+inline mycomplex
+operator- (const mycomplex& x) {
+	return mycomplex(-x.real(), -x.imag());
+}
+
+inline bool
+operator== (const mycomplex& x, const mycomplex& y) {
+	return x.real() == y.real() && x.imag() == y.imag();
+}
+
+inline bool
+operator!= (const mycomplex& x, const mycomplex& y) {
+	return x.real() != y.real() || x.imag() != y.imag();
+}
+
+#endif /* __COMPLEX_HPP__ */
